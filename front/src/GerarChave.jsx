a543e935a97044e828793d8447b81999e32453d1
Joao Falcao <joao.falcao@icloud.com>
2020-02-02 20:32:27 -0300

    localhost

--- front/src/GerarChave.jsx
+++ front/src/GerarChave.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom"
 import axios from 'axios'
 
-const baseUrl = "https://api-rsa.herokuapp.com/rsa/";
+const baseUrl = "http://127.0.0.1:5000/rsa/";
 
 class GerarChave extends React.Component {
 
